85bd1d1bab6a03e4bca2999b0e156d01e41cf7fa
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-02-06 13:24:35 +0100

    refactor(DAO): add left join in order to pass sql's team data to player objet

--- src/dao/PlayerDAO.java
+++ src/dao/PlayerDAO.java
@@ -1,12 +1,14 @@
 package dao;
 
 import models.Player;
+import models.Team;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,14 +29,13 @@ public class PlayerDAO {
                     name,
                     team,
                     score,
-                    playerId,
                     killCount,
                     deathCount
-                    VALUES (?, ?, ?, ?, ?, ?)""";
+                    VALUES (?, ?, ?, ?, ?, ?))""";
 
             try (PreparedStatement ps = con.prepareStatement(sql)) {
                 ps.setString(1, playerToAdd.getName());
-                ps.setString(2, playerToAdd.getTeam());
+                ps.setObject(2, playerToAdd.getTeam().getTeamId());
                 ps.setInt(3, playerToAdd.getScore());
                 ps.setInt(4, playerToAdd.getPlayerId());
                 ps.setInt(5, playerToAdd.getKillCount());
@@ -56,22 +57,41 @@ public class PlayerDAO {
             Connection con = dataSource.getConnection();
             Statement st = con.createStatement();
             String sql = """
-                    SELECT * FROM player""";
+                     SELECT 
+                     p.name AS player_name,
+                     t.name AS team_name,
+                     t.tag AS team_tag,
+                     t.teamId AS team_id,
+                     t.foundedDate AS team_foundedDate,
+                     p.score AS player_score,
+                     p.playerId AS player_id,
+                     p.killCount AS player_killCount,
+                     p.deathCount AS player_deathCount
+                     
+                    FROM player AS p
+                     LEFT JOIN team AS t
+                     ON
+                     p.teamIdFk = t.teamId""";
 
             ResultSet rs = st.executeQuery(sql);
 
             while (rs.next()) {
-                String name = rs.getString("name");
-                String team = rs.getString("team");
-                int score = rs.getInt("score");
-                int playerId = rs.getInt("playerId");
-                int killCount = rs.getInt("killCount");
-                int deathCount = rs.getInt("deathCount");
+                String name = rs.getString("player_name");
+                String team = rs.getString("team_name");
+                String team_tag = rs.getString("team_tag");
+                int team_id = rs.getInt("team_id");
+                LocalDate team_date = rs.getObject("team_foundedDate", LocalDate.class);
+                int score = rs.getInt("player_score");
+                int playerId = rs.getInt("player_id");
+                int killCount = rs.getInt("player_killCount");
+                int deathCount = rs.getInt("player_deathCount");
 
-                Player playerToList = new Player(name, team, score, playerId, killCount, deathCount);
+                Team t = new Team(team, team_tag, team_date, team_id);
+                Player playerToList = new Player(name, t, score, playerId, killCount, deathCount);
                 players.add(playerToList);
             }
             con.close();
+            st.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -84,7 +104,11 @@ public class PlayerDAO {
             Connection con = dataSource.getConnection();
             String sql = """
                     SELECT
-                    name, team
+                    p.name AS player_name, t.name AS team_name, t.tag AS team_tag, t.teamId AS team_id
+                    
+                    FROM player AS p
+                    LEFT JOIN team AS t 
+                    ON p.teamIdFk = t.teamId
                     WHERE playerID = ?""";
 
             try (PreparedStatement ps = con.prepareStatement(sql)) {
@@ -92,9 +116,12 @@ public class PlayerDAO {
                 ResultSet rs = ps.executeQuery();
 
                 while (rs.next()) {
-                    String name = rs.getString("name");
-                    String team = rs.getString("team");
-                    test = new Player(name, team, 0, 0, 0, 0);
+                    String name = rs.getString("player_name");
+                    String team = rs.getString("team_name");
+                    String tag = rs.getString("team_tag");
+                    int team_id = rs.getInt("team_id");
+                    Team t1 = new Team(team, tag, null, team_id);
+                    test = new Player(name, t1, 0, 0, 0, 0);
                 }
             }
             con.close();
@@ -104,18 +131,18 @@ public class PlayerDAO {
         return test;
     }
 
-    public void updatePlayer(String newName, String newTeam, int id) {
+    public void updatePlayer(String newName, Team newTeam, int id) {
 
         try {
             Connection con = dataSource.getConnection();
             String sql = """
                     UPDATE player SET
-                    name = ?, team = ?
+                    name = ?, teamIdFk = ?
                     WHERE playerId = ?""";
 
             try (PreparedStatement ps = con.prepareStatement(sql)) {
                 ps.setString(1, newName);
-                ps.setString(2, newTeam);
+                ps.setInt(2, newTeam.getTeamId());
                 ps.setInt(3, id);
 
                 ps.close();
