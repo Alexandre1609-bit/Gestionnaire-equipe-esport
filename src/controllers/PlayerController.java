a069dc65ce2522a6b8411ae34fb04bfb7a059f66
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-02-15 20:17:57 +0100

    add comment for future update

--- src/controllers/PlayerController.java
+++ src/controllers/PlayerController.java
@@ -1,6 +1,7 @@
 package controllers;
 import dao.PlayerDAO;
 import models.Player;
+import models.Team;
 import view.PlayerView;
 
 import javax.swing.text.View;
@@ -28,8 +29,9 @@ public class PlayerController {
                 case 1:
                     //Ajouter un joueur
                     String name = playerView.askName();
-                    String team = playerView.askTeam();
-                    Player player = new Player(name, team);
+                    int teamId = playerView.askTeam();
+                    String teamName = team.name.getById(id) ... //Logique du genre ? Faire la méthode dans le teamDAO
+                    Player player = new Player(name,teamName);
                     playerDAO.createPlayer(player);
                     playerView.showMessage("Joueur ajouté avec succès");
                     break;
@@ -49,10 +51,11 @@ public class PlayerController {
 
                 case 4:
                     //Modifier un joueur (nom et / ou équipe via id joueur)
-
+                    Team t1 = new Team(null, null, null);
                     String newName = playerView.askNewName();
-                    String newTeam = playerView.askNewTeamName();
-                    playerDAO.updatePlayer(newName, newTeam, 0);
+                    String  newTeam = playerView.askNewTeamName();
+                    t1.setName(newTeam);
+                    playerDAO.updatePlayer(newName, t1, 0);
                     break;
 
                 case 5:
